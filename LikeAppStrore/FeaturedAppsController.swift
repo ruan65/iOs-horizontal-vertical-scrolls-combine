db8dd1e9f7cb74577abd380fb93163984e72ac16
a <ruan65@gmail.com>
2017-04-26 02:11:41 +0300

    first row

--- LikeAppStrore/FeaturedAppsController.swift
+++ LikeAppStrore/FeaturedAppsController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class FeaturedAppsController: UICollectionViewController {
+    
+    let cellId = "category_cell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = UIColor.darkGray
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
+        return cell
+    }
+}
 
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.blue
+    }
 }
 
